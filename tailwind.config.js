fb392d318f82223949b0c028a096212d86436156
Patrick St <steiner_patrick@outlook.com>
2024-02-23 12:53:54 +0100

    created color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,40 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      indigo: {
+        light: '#a5b4fc',
+        DEFAULT: '#6366f1',
+        dark: '#3730a3',
+      },
+      amber: {
+        light: '#fcd34d',
+        DEFAULT: '#f59e0b',
+        dark: '#92400e',
+      },
+      gray: {
+        lightest: '#f5f5f4',
+        light: '#d6d3d1',
+        DEFAULT: '#78716c',
+        dark: '#292524',
+        darkest: '#0c0a09',
+      }
+    },
+    fontFamily: {
+      sans: ['Trebuchet MS', 'sans-serif'],
+      serif: ['Cambria', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   plugins: [],
 }
