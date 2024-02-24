f2db67177f2dc4b86da4c3949ca065ce9c93c259
Patrick St <steiner_patrick@outlook.com>
2024-02-24 17:09:39 +0100

    structuring the viewer by categories and not only sorting by it.

--- src/app/components/navigation/navigation.component.spec.ts
+++ src/app/components/navigation/navigation.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NavigationComponent } from './navigation.component';
 
 describe('NavigationComponent', () => {
@@ -11,7 +10,7 @@ describe('NavigationComponent', () => {
       declarations: [NavigationComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(NavigationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/components/technology-list/technology-list.component.ts
+++ src/app/components/technology-list/technology-list.component.ts
@@ -19,10 +19,23 @@ export class TechnologyListComponent {
   ngOnInit() {
     this.technologies = this.technologyService.getTechnologies()
       .filter(t => t.published)
-      .sort((a, b) => a.category < b.category ? -1 : 1)
       .sort((a, b) => a.ring < b.ring ? -1 : 1);
   }
+
+  categoryKeys(): Array<string> {
+    let keys = Object.keys(this.Category);
+    return keys.slice(keys.length / 2);
+  }
+
   selectTechnology(tech: Technology) {
     this.technologySelected.emit(tech);
+  }
+
+  getTechnologiesOfCategory(category: string) {
+    const cat = Category[category as keyof typeof Category];
+    console.log(cat);
+    return this.technologies
+      .filter(tech => tech.category === cat)
+      .sort((a, b) => a.ring < b.ring ? -1 : 1);
   }
 }
